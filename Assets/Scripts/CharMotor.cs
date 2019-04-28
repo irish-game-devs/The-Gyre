8c8353ad039ac62f217c26a62c1997af50ee9d7c
ReverendSpeed <reverendspeed@gmail.com>
2019-04-28 01:15:21 +0100

    First bits of code

--- Assets/Scripts/CharMotor.cs
+++ Assets/Scripts/CharMotor.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CharMotor : MonoBehaviour
 {
+    private const string horInputTwo = "HorizontalTwo";
+    private const string verInputTwo = "VerticalTwo";
+
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+
+    private Transform tf;
+    private Rigidbody rb;
+
+    private Vector3 desiredDir = Vector3.zero;
+
+    private void Awake()
+    {
+        tf = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(desiredDir.x) >= 0.01f || Mathf.Abs(desiredDir.z) >= 0.01f)
+        {
+            rb.MoveRotation(Quaternion.LookRotation(desiredDir));
+
+            Vector3 desiredMove = desiredDir * moveSpeed * Time.deltaTime;
+
+            rb.MovePosition(rb.position + desiredMove);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
+        desiredDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         
+        Debug.Log("Second stick hor sez wot? " + Input.GetAxis(horInputTwo));
+        Debug.Log("Second stick ver sez wot? " + Input.GetAxis(verInputTwo));        
     }
 }
---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    private Transform   tf;
+    private Rigidbody   rb;
+
+    private Transform   playerTF;
+    private Vector3     cameraOffsetFromPlayer;
+
+    private Vector3     targetPosition;
+
+    [SerializeField]
+    private float       moveSpeed = 1.0f;
+
+    private void Awake()
+    {
+        tf = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTF = GameObject.Find("Player").GetComponent<Transform>();
+
+        cameraOffsetFromPlayer = tf.position - playerTF.position;
+    }
+
+    private void FixedUpdate()
+    {
+        targetPosition = playerTF.position + cameraOffsetFromPlayer;
+
+        Vector3 desiredMove = Vector3.Lerp(rb.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        rb.MovePosition(desiredMove);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+}
