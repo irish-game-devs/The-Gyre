b2e4aff638bd8f2e4bc3dea3dcd4250e546a1eb0
stefa-no <gattos@tcd.ie>
2019-06-04 19:37:12 +0100

    removed some junk from KidSword script

--- Assets/Scripts/Weapons/KidSword.cs
+++ Assets/Scripts/Weapons/KidSword.cs
@@ -6,20 +6,6 @@ public class KidSword : Weapon
 {
     private Transform tf;
 
-    private Vector3 animationDistance = new Vector3(0f, 0.5f, 0f);
-    private float animationLength = 1.3f;
-
-    private Vector3 animStart;
-    private Vector3 animEnd;
-
-    private float animationTimer;
-    private float animationTimerLength = 0.2f;
-    private float startTime;
-
-    private float attackSpeed;
-
-    private bool attacking = false;
-
     //video variables
     private Animator animator;
     [SerializeField]
@@ -28,9 +14,6 @@ public class KidSword : Weapon
     private void Awake()
     {
         tf = GetComponent<Transform>();
-        animationTimer = animationTimerLength;
-        attackSpeed = animationLength / animationTimerLength;
-
         //video code 
         animator = GetComponent<Animator>();
     }
@@ -38,59 +21,21 @@ public class KidSword : Weapon
     void Update()
     {
         //should come from PlayerInputs
-        if (Input.GetMouseButtonDown(0) && animationTimer == animationTimerLength)
-            StartAttack();
-        else if (attacking && animationTimer <= 0)
-        {
-            animationTimer = animationTimerLength;
-            attacking = false;
-        }
-        else if (attacking && animationTimer > 0)
-        {
-            float distCovered = (Time.time - startTime) * attackSpeed;
-
-            // Fraction of journey completed = current distance divided by total distance.
-            float deltaAnimation = distCovered / animationLength;
-
-            // Set our position as a fraction of the distance between the markers.
-            Vector3 updatedStart = tf.position;
-            updatedStart.y = animStart.y;
-            Vector3 updatedEnd = tf.position;
-            updatedEnd.y = animEnd.y;
-
-            tf.position = Vector3.Lerp(updatedStart, updatedEnd, deltaAnimation);
-
-            animationTimer -= Time.deltaTime;
-
-        }
-
-
-        // from video
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
             PerformAttack();
-    }
-
-    public void StartAttack()
-    {
-        startTime = Time.time;
-        animStart = tf.position + animationDistance;
-        animEnd = tf.position;
-        tf.position = animEnd;
-        attacking = true;
-
     }
 
     public void PerformAttack()
     {
-        //res: https://www.youtube.com/watch?v=HrNebvxSUsU
-        //notes from video: the animation are gonna be handled by the weapon and 
+        // res: https://www.youtube.com/watch?v=HrNebvxSUsU
+        // notes from video: the animation are gonna be handled by the weapon and 
         // not by the hand. This is because the animation of each weapon my be different
         // and it might also change weather it's in idle, fight stance etc
         // so each weapon is gonna have it's own animator controller and the animation is gonna
         // be confined in the weapon. The hand is gonna be there just kinda like a bone to attach stuff 
         // to (expect I don't have all he has yet probably). 
         // In any case since we will probably drive through the character animation in the end in our actual
-        //build
+        // build
 
         //on KidSword prefab setting Is Trigger = true prevents the sword to affect the physics 
         //(doesn'thit the walls/cars, doesn't push stuff around).
@@ -114,6 +59,8 @@ public class KidSword : Weapon
         if (other.tag == "Enemy")
         {
             //do stuff inflict damage
+            // should instead get something like a Enemy script from the other.gameObject
+            // and call some type of Enemy.ReceiveDamage() method
             Destroy(other.gameObject);
         }
     }
